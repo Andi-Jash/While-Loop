b512b7c60d04e30a7249654b7aba4f4db0281b99
Andi Jashari/Kurs <an0nymouscrature@gmail.com>
2024-07-16 12:14:07 +0200

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,8 @@
+while True:
+    emri = input("Sheno username:")
+    if emri != "andijashari":
+        break
+    password = input("Shkruaje passwordin:")
+    if password != "jcoders":
+        break
+    print("Emri dhe Passwordi jane ne rregull")
